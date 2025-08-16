65d77e7b2a207f6e4dc5a33d7f051e2114982a1f
Vlad Ilie <vladflorin.ilie@gmail.com>
2025-08-16 16:03:04 +0300

    update dependencies

--- src/setup.py
+++ src/setup.py
@@ -2,15 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='YeelightMatrix',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     package_data={
         "custom_components.yeelight_matrix": ["*.json", "translations/*.json"],
     },
     include_package_data=True,
     install_requires=[
-        'yeelight==0.7.14',
-        'Pillow==11.0.0',
-        'setuptools==65.5.0'
+        'yeelight>=0.7.14',
+        'Pillow>=11.0.0'
     ],
 )
